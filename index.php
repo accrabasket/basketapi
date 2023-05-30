0e536bcb682a83fcdc8a46b4638039614d379b0c
root <root@ubuntu.members.linode.com>
2023-05-30 16:55:31 +0000

    api commit

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
 }
-
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
 // Setup autoloading
+
 require 'init_autoloader.php';
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
